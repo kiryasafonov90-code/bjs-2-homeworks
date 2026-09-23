04d22050cb82451f86b8f0eea111c1291460ec15
Safonov <kirya.safonov.90@gmail.com>
2026-09-23 14:50:27 +0300

    Решить задачи по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter((user) => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const ages = filteredUsers.map((user) => user.age);
+
+  const sum = ages.reduce((total, age) => total + age, 0);
+
+  return sum / ages.length;
 }
